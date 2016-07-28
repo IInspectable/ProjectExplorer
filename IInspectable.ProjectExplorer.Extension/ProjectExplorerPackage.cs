fbd4685e14da9496589462f5c64fc44ac3e82610
IInspectable <wakok@gmx.de>
2016-07-28 19:39:56 +0200

    Default DockStyle

--- IInspectable.ProjectExplorer.Extension/ProjectExplorerPackage.cs
+++ IInspectable.ProjectExplorer.Extension/ProjectExplorerPackage.cs
@@ -11,7 +11,8 @@ namespace IInspectable.ProjectExplorer.Extension {
     [PackageRegistration(UseManagedResourcesOnly = true)]
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
     [ProvideMenuResource("Menus.ctmenu", 1)]
-    [ProvideToolWindow(typeof(ProjectExplorerWindow))]
+    [ProvideToolWindow(typeof(ProjectExplorerWindow), Style = VsDockStyle.Tabbed, Window = "3ae79031-e1bc-11d0-8f78-00a0c9110057")]
+    [ProvideToolWindowVisibility(typeof(ProjectExplorerWindow), /*UICONTEXT_SolutionExists*/ "f1536ef8-92ec-443c-9ed7-fdadf150da82")]
     [Guid(PackageGuidString)]
     public sealed class ProjectExplorerPackage : Package {
 
