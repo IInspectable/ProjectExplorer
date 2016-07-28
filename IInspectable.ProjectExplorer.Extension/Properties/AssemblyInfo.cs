d1201c042e70c43509dc61930a64688a6c5c378b
IInspectable <wakok@gmx.de>
2016-07-28 18:28:39 +0200

    Project renamed

--- IInspectable.ProjectExplorer.Extension/Properties/AssemblyInfo.cs
+++ IInspectable.ProjectExplorer.Extension/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("IInspectable.ProjectAdder")]
+[assembly: AssemblyTitle("IInspectable.ProjectExplorer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("IInspectable.ProjectAdder")]
+[assembly: AssemblyProduct("IInspectable.ProjectExplorer")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
