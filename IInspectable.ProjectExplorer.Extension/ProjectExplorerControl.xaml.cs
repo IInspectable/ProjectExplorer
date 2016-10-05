6430876482a12b140da0494fe8c65314b4f80557
IInspectable <wakok@gmx.de>
2016-10-05 12:04:43 +0200

    Projektliste Wurf1

--- IInspectable.ProjectExplorer.Extension/ProjectExplorerControl.xaml.cs
+++ IInspectable.ProjectExplorer.Extension/ProjectExplorerControl.xaml.cs
@@ -1,12 +1,24 @@
 ﻿#region Using Directives
 
+using System.Collections.Generic;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 
 #endregion
 
 namespace IInspectable.ProjectExplorer.Extension {
-    
+
+    public class ProjectFile {
+
+        public ProjectFile(string name, string path) {
+            Name = name;
+            Path = path;
+        }
+
+        public string Name { get; }
+        public string Path { get; }
+    }
 
     public partial class ProjectExplorerControl : UserControl {
 
@@ -15,6 +27,19 @@ namespace IInspectable.ProjectExplorer.Extension {
         }
 
         void OnButtonClick(object sender, RoutedEventArgs e) {
+
+            var path = @"C:\ws\Roslyn";
+
+            var projectFiles = new List<ProjectFile>();
+
+            foreach(var file in Directory.EnumerateFiles(path, "*.csproj", SearchOption.AllDirectories)) {
+                var projectFile = new ProjectFile(Path.GetFileNameWithoutExtension(file), file);
+
+                projectFiles.Add(projectFile);
+            }
+
+            DataContext = projectFiles;
+
             MessageBox.Show(
                 string.Format(System.Globalization.CultureInfo.CurrentUICulture, "Invoked '{0}'", ToString()),
                 "ProjectExplorerWindow");
