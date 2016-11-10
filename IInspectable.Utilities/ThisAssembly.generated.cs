9cf0c1f20ffe5cfaac4ece51fd10bc90771ac97d
IInspectable <github@smjh.de>
2016-11-10 13:37:50 +0100

    Version Number 1.2

--- IInspectable.Utilities/ThisAssembly.generated.cs
+++ IInspectable.Utilities/ThisAssembly.generated.cs
@@ -1,7 +1,7 @@
 // Warning: This is generated code! Don't touch as it will be overwritten by the code generator.
 namespace IInspectable.Utilities {
     static partial class ThisAssembly {
-        public const string ProductVersion = "1.1.11";
+        public const string ProductVersion = "1.2.0";
         public const string ProductName = "IInspectable Utilities";
     }
 }
