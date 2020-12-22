137b94f9cfa641370b51f8a9f29fd8801fd4ff32
IInspectable <github@smjh.de>
2020-12-22 11:56:53 +0100

    Support SDK Style Projects

--- IInspectable.ProjectExplorer.Extension/Model/ProjectFile.cs
+++ IInspectable.ProjectExplorer.Extension/Model/ProjectFile.cs
@@ -1,7 +1,5 @@
 ﻿#region Using Directives
 
-using System;
-using System.IO;
 using System.Xml;
 
 #endregion
@@ -10,14 +8,12 @@ namespace IInspectable.ProjectExplorer.Extension {
 
     public class ProjectFile {
 
-        ProjectFile(string name, string path, string assemblyName, Guid projectGuid) {
+        ProjectFile(string name, string path, string assemblyName) {
             Name         = name;
             Path         = path;
             AssemblyName = assemblyName;
-            ProjectGuid  = projectGuid;
         }
 
-        public Guid ProjectGuid { get; }
         public string Name { get; }
         public string Path { get; }
         public string AssemblyName { get; set; }
@@ -28,26 +24,15 @@ namespace IInspectable.ProjectExplorer.Extension {
 
             xmlDocument.Load(fileName + "");
 
-            if (xmlDocument.DocumentElement?.NamespaceURI != "http://schemas.microsoft.com/developer/msbuild/2003") {
-                throw new FileLoadException($"\'{fileName}' is not a supported MSBuild project file");
-            }
-
             var assemblyName = "";
             var assemblyNameElement = xmlDocument.GetElementsByTagName("AssemblyName");
             if (assemblyNameElement.Count > 0) {
                 assemblyName = assemblyNameElement[0].FirstChild.Value;
             }
-
-            var projectGuid = Guid.Empty;
-            var projectGuidElement = xmlDocument.GetElementsByTagName("ProjectGuid");
-            if(projectGuidElement.Count > 0) {
-                projectGuid = Guid.Parse(projectGuidElement[0].FirstChild.Value);
-            }
             
             return new ProjectFile(name        : System.IO.Path.GetFileNameWithoutExtension(fileName), 
                                    path        : fileName, 
-                                   assemblyName: assemblyName, 
-                                   projectGuid : projectGuid);
+                                   assemblyName: assemblyName);
         }
     }
 }
